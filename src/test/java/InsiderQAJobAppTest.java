82fc32f22383d4286065a27df6ebe9c208fcd4bc
kenankunc <kenankunc@gmail.com>
2023-04-10 01:30:58 +0300

    Test Class name is changed

--- src/test/java/InsiderQAJobAppTest.java
+++ src/test/java/InsiderQAJobAppTest.java
@@ -1,20 +1,15 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 
-import javax.swing.*;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class homePageTest extends BrowserDriver  {
+public class InsiderQAJobAppTest extends BrowserDriver  {
     private String expectedGetPlatformText;
     private String actualGetPlatformText;
 
